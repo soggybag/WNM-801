f628c30590e7b987d7bb25059e7290d1743dcdf3
Mitchell Hudson <soggybag@gmail.com>
2016-02-26 15:46:27 -0800

    update

---
+++ Animated-Banner/images.js
@@ -0,0 +1,19 @@
+function BannerImage(name, w, h, desc) {
+    this.name = name;
+    this.width = w;
+    this.height = h;
+    this.desc = desc;
+}
+
+var images = [];
+images.push(new BannerImage("IMG_0133.jpg", 240, 220, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_0134.jpg", 240, 220, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_0138.jpg", 200, 200, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_0139.jpg", 160, 270, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_0140.jpg", 240, 240, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_1056.jpg", 240, 320, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_1057.jpg", 240, 320, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_1058.jpg", 240, 320, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_1059.jpg", 240, 320, "<h1>Hello</h1><p>Hello world.</p>"));
+images.push(new BannerImage("IMG_1060.jpg", 240, 320, "<h1>Hello</h1><p>Hello world.</p>"));
+
